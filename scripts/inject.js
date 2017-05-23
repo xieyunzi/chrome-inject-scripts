f103742b4c3307ca8451fd52fdf1c5600f37bcaf
xieyunzi <xieyunzi@foxmail.com>
2017-05-23 11:51:17 +0800

    add cheerio

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,11 +1,13 @@
 import R from 'ramda'
 import _ from 'lodash'
 import $ from 'jquery'
+import cheerio from 'cheerio'
 
 // inject for browser console
 const inject = {
   R: R,
   _: _,
   $: $,
+  cheerio: cheerio,
 }
 window.xzinject = inject
